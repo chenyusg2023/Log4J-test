fab8de57a84bf4225c2b7bafc87fe29433f9e9b5
Akshay Raj Gollahalli <akshay@gollahalli.com>
2016-05-25 17:28:28 +1200

    code updated

--- src/com/gollahalli/UsingLog4JSL4J.java
+++ src/com/gollahalli/UsingLog4JSL4J.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gollahalli;
 
 import java.nio.file.Path;
@@ -13,32 +8,23 @@ import org.apache.logging.log4j.LogManager;
 
 /**
  *
- * @author akshayrajgollahalli
+ * @author Akshay Raj Gollahalli
  */
 public class UsingLog4JSL4J {
-    
-//    static String getUserAppDirectory() {
-//        String osName = System.getProperty("os.name");
-//        if (osName.contains("Mac")) {
-//            return System.getProperty("user.home") + "/.JCal/logs/JCal-log.log";
-//        }
-//        else {
-//            return System.getProperty("user.home") + "/.JCal/logs/JCal-log.log";
-//        }
-//    }
     
     static {
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
+        
+        // adding the location to the log file, if the log file is not already present
+        // a log file with LOG4JSL4J.log is created
         System.setProperty("LOG4JSL4J", s + "/LOG4JSL4J.log");
+        // Location of the log4j config file
         System.setProperty("log4j.configurationFile", "com/gollahalli/log4j-configFile.xml");        
     }
     
     public static final Logger logger = LogManager.getLogger(UsingLog4JSL4J.class);
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         logger.error("This is an error");
         logger.info("this is info log");
